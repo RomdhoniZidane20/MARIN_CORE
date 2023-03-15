112f547785ce3b48b10a52b56e6075530a64028d
RomdhoniZidane20 <zidanespada@gmail.com>
2023-03-16 02:15:15 +0700

    add accel gyro data and rearanged address

--- firmware/Marin_Core_Firmware/Marin_Core_Firmware.ino
+++ firmware/Marin_Core_Firmware/Marin_Core_Firmware.ino
@@ -6,7 +6,7 @@
 #include <utility/imumaths.h>
 
 //IMU variables
-uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 0;
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 Madgwick filter;
 
@@ -39,26 +39,25 @@ const uint16_t DXL_MODEL_NUM     = 0x190; // OpenCM 9.04 model number
 DYNAMIXEL::Slave dxl(dxl_port, DXL_MODEL_NUM);
 
 
-// Addresses for read register variable
-uint8_t SW_Start_addr    = 60;
-uint8_t SW_Stop_addr     = 62;
-uint8_t Yaw_addr         = 64;
-uint8_t Pitch_addr       = 66;
-uint8_t Roll_addr        = 68;
-uint8_t Reset_addr       = 70;
-
 // Address for wirte register variable
-uint8_t dxl_enbale_addr  = 72;
-uint8_t LED_R_addr       = 74;
-uint8_t LED_G_addr       = 76;
-uint8_t LED_B_addr       = 78;
+uint8_t Reset_addr       = 55;
+uint8_t dxl_enbale_addr  = 56;
+uint8_t LED_R_addr       = 57;
+uint8_t LED_G_addr       = 58;
+uint8_t LED_B_addr       = 59;
 
-//uint16_t AccelX_addr      = 66;
-//uint16_t AccelY_addr      = 74;
-//uint16_t AccelZ_addr      = 76;
-//uint16_t GyroX_addr       = 78;
-//uint16_t GyroY_addr       = 76;
-//uint16_t GyroZ_addr       = 78;
+// Addresses for read register variable
+uint16_t SW_Start_addr    = 60;
+uint16_t SW_Stop_addr     = 62;
+uint16_t Yaw_addr         = 64;
+uint16_t Pitch_addr       = 66;
+uint16_t Roll_addr        = 68;
+uint16_t AccelX_addr      = 70;
+uint16_t AccelY_addr      = 72;
+uint16_t AccelZ_addr      = 74;
+uint16_t GyroX_addr       = 76;
+uint16_t GyroY_addr       = 78;
+uint16_t GyroZ_addr       = 80;
 
 // Stored variables for Interrupt
 uint8_t Switch_Button[2];
@@ -66,6 +65,7 @@ uint8_t dxl_power_enable;
 
 // Stored variable for imu
 uint16_t dxl_data[20];
+//float dxl_data_f[12];
 uint16_t accel[3];
 uint16_t gyro[3];
 
@@ -84,13 +84,6 @@ void setup() {
 
   DEBUG_SERIAL.begin(115200);
   bno.begin(bno.OPERATION_MODE_IMUPLUS);
-  //  if (!bno.begin(bno.OPERATION_MODE_IMUPLUS))
-  //  {
-  //    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-  //    while (1);
-  //}
-
-  //  bno.setMode(bno.OPERATION_MODE_IMUPLUS);
 
   pinMode(SW_Start, INPUT);
   pinMode(SW_Stop, INPUT);
@@ -105,29 +98,29 @@ void setup() {
   dxl.setFirmwareVersion(24);
   dxl.setID(DXL_ID);
 
-  dxl.addControlItem(SW_Start_addr,   dxl_data[0]);
-  dxl.addControlItem(SW_Stop_addr,    dxl_data[1]);
-  dxl.addControlItem(Yaw_addr,        dxl_data[2]);
-  dxl.addControlItem(Pitch_addr,      dxl_data[3]);
-  dxl.addControlItem(Roll_addr,       dxl_data[4]);
-  dxl.addControlItem(Reset_addr,      dxl_data[5]);
-  dxl.addControlItem(dxl_enbale_addr, dxl_data[6]);
-  dxl.addControlItem(LED_R_addr,      dxl_data[7]);
-  dxl.addControlItem(LED_G_addr,      dxl_data[8]);
-  dxl.addControlItem(LED_B_addr,      dxl_data[9]);
+  dxl.addControlItem(Reset_addr,      dxl_data[0]);
+  dxl.addControlItem(dxl_enbale_addr, dxl_data[1]);
+  dxl.addControlItem(LED_R_addr,      dxl_data[2]);
+  dxl.addControlItem(LED_G_addr,      dxl_data[3]);
+  dxl.addControlItem(LED_B_addr,      dxl_data[4]);
+  dxl.addControlItem(SW_Start_addr,   dxl_data[5]);
+  dxl.addControlItem(SW_Stop_addr,    dxl_data[6]);
+  dxl.addControlItem(Yaw_addr,        dxl_data[7]);
+  dxl.addControlItem(Pitch_addr,      dxl_data[8]);
+  dxl.addControlItem(Roll_addr,       dxl_data[9]);
+  dxl.addControlItem(AccelX_addr,     dxl_data[10]);
+  dxl.addControlItem(AccelY_addr,     dxl_data[11]);
+  dxl.addControlItem(AccelZ_addr,     dxl_data[12]);
+  dxl.addControlItem(GyroX_addr,      dxl_data[13]);
+  dxl.addControlItem(GyroY_addr,      dxl_data[14]);
+  dxl.addControlItem(GyroZ_addr,      dxl_data[15]);
 
-  //  dxl.addControlItem(AccelX_addr, accel[0]);
-  //  dxl.addControlItem(AccelY_addr, accel[1]);
-  //  dxl.addControlItem(AccelZ_addr, accel[2]);
-  //  dxl.addControlItem(GyroX_addr, gyro[0]);
-  //  dxl.addControlItem(GyroY_addr, gyro[1]);
-  //  dxl.addControlItem(GyroZ_addr, gyro[2]);
 
 
   dxl.setReadCallbackFunc(read_callback_func);
   dxl.setWriteCallbackFunc(write_callback_func);
 
-  //  delay();
+  //    delay();
 
 }
 
@@ -142,29 +135,33 @@ void loop() {
     DEBUG_SERIAL.println(dxl.getLastStatusPacketError());
     DEBUG_SERIAL.println();
 
-    //    delayMicroseconds(Return_Delay);
-
   }
 
   imu::Vector<3> Orientation = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  //  imu::Vector<3> Accel       = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  //  imu::Vector<3> Gyro        = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu::Vector<3> Accel       = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> Gyro        = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
   //  YPR value
-  dxl_data[2]  = Orientation.x();
-  dxl_data[3]  = Orientation.y() + 360;
-  dxl_data[4]  = Orientation.z() + 360;
+  dxl_data[7]  = Orientation.x();
+  dxl_data[8]  = Orientation.y() + 360;
+  dxl_data[9]  = Orientation.z() + 360;
+
   //  dxl_data[5]  = random(200);
 
-  //  //  Accelerometer Value
-  //  accel[0]    = Accel.x();
-  //  accel[1]    = Accel.y();
-  //  accel[2]    = Accel.z();
-  //
-  //  //  Gyro Value
-  //  gyro[0]    = Gyro.x();
-  //  gyro[1]    = Gyro.y();
-  //  gyro[2]    = Gyro.z();
+  //  Accelerometer Value
+  dxl_data[10]    = Accel.x() + 100;
+  dxl_data[11]    = Accel.y() + 100;
+  dxl_data[12]    = Accel.z() + 100;
+
+  //  Gyro Value
+  dxl_data[13]    = Gyro.x() + 100;
+  dxl_data[14]    = Gyro.y() + 100;
+  dxl_data[15]    = Gyro.z() + 100;
+
+  //  DEBUG_SERIAL.print("Accel X: ");
+  //  DEBUG_SERIAL.print(dxl_data_f[0]);
+  //    Serial.print("Accel Y: ");
+  //    Serial.print("Accel Z: ");
 
   //  random_num = random(200);
 
@@ -177,6 +174,7 @@ void loop() {
   bno.Calibration_no_mag(&system, &gyro, &accel);
 
   //  delayMicroseconds(Return_Delay);
+  //  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
 void read_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
@@ -186,59 +184,59 @@ void read_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
   if (item_addr == SW_Start_addr)
   {
     start_button_state = digitalRead(SW_Start);
-    dxl_data[0] = start_button_state;
+    dxl_data[5] = start_button_state;
   }
 
   if (item_addr == SW_Stop_addr)
   {
     stop_button_state = digitalRead(SW_Stop);
-    dxl_data[1] = stop_button_state;
+    dxl_data[6] = stop_button_state;
   }
 
   if (item_addr == Yaw_addr)
   {
-    dxl_data[2];
+    dxl_data[7];
   }
 
   if (item_addr == Pitch_addr)
   {
-    dxl_data[3];
+    dxl_data[8];
   }
 
   if (item_addr == Roll_addr)
   {
-    dxl_data[4];
+    dxl_data[9];
   }
 
-  //  if (item_addr == AccelX_addr)
-  //  {
-  //    accel[0];
-  //  }
-  //
-  //  if (item_addr == AccelY_addr)
-  //  {
-  //    accel[1];
-  //  }
-  //
-  //  if (item_addr == AccelZ_addr)
-  //  {
-  //    accel[2];
-  //  }
-  //
-  //  if (item_addr == GyroX_addr)
-  //  {
-  //    gyro[0];
-  //  }
-  //
-  //  if (item_addr == GyroY_addr)
-  //  {
-  //    gyro[1];
-  //  }
-  //
-  //  if (item_addr == GyroZ_addr)
-  //  {
-  //    gyro[2];
-  //  }
+  if (item_addr == AccelX_addr)
+  {
+    dxl_data[10];
+  }
+
+  if (item_addr == AccelY_addr)
+  {
+    dxl_data[11];
+  }
+
+  if (item_addr == AccelZ_addr)
+  {
+    dxl_data[12];
+  }
+
+  if (item_addr == GyroX_addr)
+  {
+    dxl_data[13];
+  }
+
+  if (item_addr == GyroY_addr)
+  {
+    dxl_data[14];
+  }
+
+  if (item_addr == GyroZ_addr)
+  {
+    dxl_data[15];
+  }
 
 }
 
@@ -249,7 +247,7 @@ void write_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
 
   if (item_addr == Reset_addr)
   {
-    if (dxl_data[5] == 1)
+    if (dxl_data[0] == 1)
     {
       ESP.restart();
     }
@@ -258,21 +256,21 @@ void write_callback_func(uint16_t item_addr, uint8_t &dxl_err_code, void* arg)
   if (item_addr == dxl_enbale_addr)
   {
 
-    digitalWrite(Dxl_power, dxl_data[6]);
+    digitalWrite(Dxl_power, dxl_data[1]);
   }
 
   if (item_addr == LED_R_addr)
   {
-    digitalWrite(LED_Red_enable, dxl_data[7]);
+    digitalWrite(LED_Red_enable, dxl_data[2]);
   }
 
   if (item_addr == LED_G_addr)
   {
-    digitalWrite(LED_Green_enable, dxl_data[8]);
+    digitalWrite(LED_Green_enable, dxl_data[3]);
   }
 
   if (item_addr == LED_B_addr)
   {
-    digitalWrite(LED_Blue_enable, dxl_data[9]);
+    digitalWrite(LED_Blue_enable, dxl_data[4]);
   }
 }
